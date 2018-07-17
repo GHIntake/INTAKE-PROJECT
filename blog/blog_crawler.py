fc7054acba7370de373336c243f00722e94e7116
Youkyoung Cha <yougyoung94@gmail.com>
2018-07-17 10:49:12 +0900

    tried blog.me failed TTT

--- blog/blog_crawler.py
+++ blog/blog_crawler.py
@@ -19,10 +19,12 @@ driver.get('https://search.naver.com/search.naver?sm=tab_hty.top&where=post&quer
 
 
 
-conn = pymssql.connect("intakedb.c63elkxbiwfc.us-east-2.rds.amazonaws.com:1433", "gh", "ghintake", "intake")
-cursor = conn.cursor()
+#conn = pymssql.connect("intakedb.c63elkxbiwfc.us-east-2.rds.amazonaws.com:1433", "gh", "ghintake", "intake")
+#cursor = conn.cursor()
 
 def datetime(x):
+    # if len(x)<=7:
+    #     return datetime.datime.now()
     y = x.split('. ')
     if len(y[1])==1:
         y[1] = '0'+y[1]
@@ -36,7 +38,11 @@ def datetime(x):
     z = y[0]+'-'+y[1]+'-'+y[2]+' '+ y[3]
     return z
 
-for j in range(1):
+date_need_preprocessing = pd.DataFrame()
+
+##  blog.me는 형식이 다 달라서 안 되겠다 때려친다!
+
+for j in range(2):
     total_blog = pd.DataFrame()
     post_links = driver.find_elements_by_css_selector('#elThumbnailResultArea > li > dl > dt > a')
     for i in post_links:
@@ -45,12 +51,12 @@ for j in range(1):
             i.click()
             driver.switch_to.window(driver.window_handles[1])
             time.sleep(3)
-            driver.switch_to.frame(driver.find_element_by_css_selector('#mainFrame'))
-            blog_post_name = driver.find_element_by_css_selector('div:nth-child(3) > div > div > div.pcol1 > div.se_editArea > div > div > div > h3')
+            driver.switch_to.frame(driver.find_element_by_css_selector('#ScreenFrame')) #blog.naver.com : #mainFrame
+            blog_post_name = driver.find_element_by_css_selector('div:nth-child(3) > div > div.se_sectionArea.is-fill.se_align-left > div.pcol1 > div.se_editArea > div > div > div > h3') #blog.naver.com : 'div:nth-child(3) > div > div > div.pcol1 > div.se_editArea > div > div > div > h3'
             post_name = blog_post_name.text.strip()
-            maintext = driver.find_element_by_css_selector('div.se_doc_viewer > div.se_component_wrap.sect_dsc.__se_component_area')
+            maintext = driver.find_element_by_css_selector('div.se_component_wrap.sect_dsc.__se_component_area') #blog.naver.com : div.se_doc_viewer > div.se_component_wrap.sect_dsc.__se_component_area
             sentences = maintext.text.strip()
-            blog_created_at = driver.find_element_by_css_selector('div:nth-child(3) > div > div > div.se_container > span')
+            blog_created_at = driver.find_element_by_css_selector('div:nth-child(3) > div > div.se_sectionArea.is-fill.se_align-left > div.se_container > span') #blog.naver.com : div:nth-child(3) > div > div > div.se_container > span
             created_at = blog_created_at.text.strip()
             page = driver.current_url
             blog_current_url = driver.current_url
@@ -72,7 +78,10 @@ for j in range(1):
 
             print(temp_row)
 
-            total_blog = total_blog.append(temp_row, ignore_index=True)
+            if len(created_at) <= 8:
+                date_need_preprocessing = date_need_preprocessing.append(temp_row, ignore_index=True)
+            else:
+                total_blog = total_blog.append(temp_row, ignore_index=True)
 
         except NoSuchElementException:
             driver.close()
@@ -94,7 +103,8 @@ for j in range(1):
     cursor.executemany("insert into NaverBlogReview (created_at, current_url, keyword, main_text, post_name) values(%s, %s, %s, %s, %s)", data)
     conn.commit()
 
-total_blog = total_blog[['keyword', 'created_at', 'post_name', 'main_text', 'current_url']]
+date_need_preprocessing = date_need_preprocessing[['keyword', 'created_at', 'post_name', 'main_text', 'current_url']]
+#total_blog = total_blog[['keyword', 'created_at', 'post_name', 'main_text', 'current_url']]
 # print(total_blog)
 
 
@@ -106,5 +116,15 @@ total_blog = total_blog[['keyword', 'created_at', 'post_name', 'main_text', 'cur
 # 데이터베이스에 insert
 
 # 최종 commit
+
+conn.close()
+
+conn = pymssql.connect("intakedb.c63elkxbiwfc.us-east-2.rds.amazonaws.com:1433", "gh", "ghintake", "intake")
+cursor = conn.cursor()
+
+data = date_need_preprocessing.values.tolist()
+data = [tuple(lst) for lst in data]
+cursor.executemany("insert into NaverBlogReview (keyword, created_at, post_name, main_text, current_url) values(%s, %s, %s, %s, %s)", data)
+conn.commit()
 
 conn.close()
